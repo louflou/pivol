9d61d8a799e9b13ced3e14e401e539a908a673a6
Awni <Wintrygamer@gmail.com>
2018-03-18 20:13:40 +0100

    AJAX update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,41 @@
 $(document).ready(function() {
 
     let searchInput = $("#searchInput");
-    let searchBtn = $("searchBtn");
+    let searchBtn = $("#searchBtn");
+
+    searchBtn.on("click", function() {
+        //searchBeer();
+
+        //let beerId = $("#searchInput").val();
+        //let beerName = searchInput.value();
+        const key = "cc12540abedfa669021307d4ba111d87";
+        let beerName = $("#searchInput").val();
+        let path = "https://api.brewerydb.com/v2/";
+        let myData = 'search?q=' + beerName + '&type=beer&key=' + key; //data to be sent
+        console.log(path+myData);
+
+        jQuery.ajax({
+            type: 'GET', // POST or GET
+            url: path, //Where to make Ajax calls aka route
+            dataType: 'jsonp', // Data type, HTML, json etc.
+            data: myData, //Form variables
+            timeout: 500,
+            beforeSend: function () {
+                //console.log('loading')
+            },
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log(thrownError);
+            }
+        });
+
+    });
 
 
-    $("#searchBtn").on("click", function() {
+
+    function searchBeer () {
 
         //let beerId = $("#searchInput").val();
         //let beerName = searchInput.value();
@@ -16,7 +47,7 @@ $(document).ready(function() {
     	$.ajax({
             type: 'GET', // POST or GET
             url: path, //Where to make Ajax calls aka route
-            dataType: 'json', // Data type, HTML, json etc.
+            dataType: 'jsonp', // Data type, HTML, json etc.
             data: myData, //Form variables
             timeout: 500,
     	}).done(function (data){
@@ -27,12 +58,13 @@ $(document).ready(function() {
     		$("#tips").text("Det gick inte att hämta ett tips.");
     	});
 
-    });
+    }
 
+    function displayError() {
 
+    }
 
-    function searchBeer () {
-
+    function displaySucess() {
 
     }
 
