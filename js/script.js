8f1da35d95fedaa4a8622fa1e64a1d838c4fc992
Awni <Wintrygamer@gmail.com>
2018-03-20 04:59:41 +0100

    S

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@ $(document).ready(function() {
         let row = $('<div class="row align-items-center item"> </div>');
         row.append('<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 picture"><img src="' + imgSrc + '" alt="beer icon" class="img-thumbnail" /></div>');
         row.append('<div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5 name"><a class="itemLink" data-beer-id="' + beerId + '" href="#">' + name + '</a></div>');
-        row.append('<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 volume"><p>' + volume + '%</p></div>');
+        row.append('<div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 volume"><p>' + volume + ' %</p></div>');
         $('.results').append(row);
     }
 
